23a371b15ea334bb58f955782ad68ff0320ee032
faisal ahmed <mishu_dc@yahoo.com>
2018-02-03 22:50:02 -0500

    Single Number (XOR)

---
+++ Solutions/SingleNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class SingleNumber
+    {
+        public int Execute(int[] nums)
+        {
+            int i = 0;
+
+            Array.Sort(nums);
+            for(i=0;i<nums.Length-1;i=i+2)
+            {
+                if(nums[i]!=nums[i+1])
+                {
+                    return nums[i];
+                }
+            }
+            return nums[i];
+        }
+
+        public int ExecuteUsingXor(int[] nums)
+        {
+            int i = 0;
+            int result = 0;
+
+            Array.Sort(nums);
+            for (i = 0; i < nums.Length; i++)
+            {
+                result ^= nums[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTests/SingleNumberTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SingleNumberTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SingleNumber number = new SingleNumber();
+            int result = number.ExecuteUsingXor(new int[] { 1, 2, 3, 1, 2, 3, 5 });
+            Assert.AreEqual(result, 5);
+        }
+    }
+}
