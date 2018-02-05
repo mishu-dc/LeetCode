01e82b2034630f262d0173e1477b2d153253aa2f
faisal ahmed <mishu_dc@yahoo.com>
2018-02-04 21:29:43 -0500

    Valid Palindrome

---
+++ Solutions/ValidPalindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            char[] values = s.Trim().ToLower().ToCharArray();
+
+            int i = 0, j = values.Length - 1;
+
+            while (i < j)
+            {
+                while (i < values.Length && !char.IsLetterOrDigit(values[i])) i++;
+                while (j >= 0 && !char.IsLetterOrDigit(values[j])) j--;
+
+                if (i > j) return true;
+
+                if (values[i] != values[j]) return false;
+                i++;
+                j--;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ UnitTests/ValidPalindromeTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ValidPalindromeTest
+    {
+        [TestMethod]
+        public void ValidPalindromeTestmethod1()
+        {
+            ValidPalindrome pd = new ValidPalindrome();
+            bool result = pd.IsPalindrome("   abc   ba");
+            Assert.IsTrue(result);
+
+            result = pd.IsPalindrome("   ; ; ; ; ; ,");
+            Assert.IsTrue(result);
+
+            result = pd.IsPalindrome("   ");
+            Assert.IsTrue(result);
+
+            result = pd.IsPalindrome("A man, a plan, a canal: Panama");
+            Assert.IsTrue(result);
+
+            result = pd.IsPalindrome("race a car");
+            Assert.IsFalse(result);
+        }
+    }
+}
