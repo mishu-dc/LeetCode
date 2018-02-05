a4a156cffd1010ce9ae0586ded61d4d03ccc98f3
faisal ahmed <mishu_dc@yahoo.com>
2018-02-04 22:02:26 -0500

    ImplementStrStr

---
+++ Solutions/ImplementStrStr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class ImplementStrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            char[] hayStackChars = haystack.ToCharArray();
+            char[] needleChars = needle.ToCharArray();
+
+            for(int i=0; ;i++)
+            {
+                for(int j=0; ;j++)
+                {
+                    if (j == needle.Length) return i;
+                    if (i + j == hayStackChars.Length) return -1;
+                    if (needleChars[j] != hayStackChars[i + j]) break;
+                }
+            }
+        }
+    }
+}
---
+++ UnitTests/ImplementStrStrTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ImplementStrStrTest
+    {
+        [TestMethod]
+        public void ImplementStrStrTestMethod1()
+        {
+            ImplementStrStr strstr = new ImplementStrStr();
+            int result = strstr.StrStr("xxbc", "bc");
+            Assert.AreEqual(result, 2);
+
+            result = strstr.StrStr("xxxx", "bc");
+            Assert.AreEqual(result, -1);
+        }
+    }
+}
