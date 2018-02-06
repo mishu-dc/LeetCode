6ae8b4dbca6d8b5668c96e4b5de491d6270768fb
faisal ahmed <mishu_dc@yahoo.com>
2018-02-06 00:33:00 -0500

    JewelsInStones

---
+++ Solutions/JewelsAndStones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class JewelsAndStones
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+            
+            char[] jewelChars = J.ToCharArray();
+            char[] stones = S.ToCharArray();
+            bool[] jewels = new bool[256];
+
+            foreach (char j in jewelChars)
+                jewels[j]=true;
+
+            foreach (char s in stones)
+                if (jewels[s])
+                    count++;
+            
+            return count;
+        }
+    }
+}
---
+++ UnitTests/JewelsInStonesTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class JewelsInStonesTest
+    {
+        [TestMethod]
+        public void NumJewelsInStonesTestMethod1()
+        {
+            JewelsAndStones js = new JewelsAndStones();
+            int result = js.NumJewelsInStones("aAbB", "aaaaabB");
+            Assert.AreEqual(result, 7);
+        }
+    }
+}
