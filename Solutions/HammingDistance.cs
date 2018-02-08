dc8235a00eed71799cf4da3b8fa746f9cb7c685d
faisal ahmed <mishu_dc@yahoo.com>
2018-02-08 01:16:31 -0500

    Hamming Distance

---
+++ Solutions/HammingDistance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class HammingDistance
+    {
+        public int Solution(int x, int y)
+        {
+            int count = 0;
+            int i = 0;
+
+            char[] smaller = GetBits(x < y ? x : y);
+            char[] bigger = GetBits(x > y ? x : y);
+
+            for (i = 0; i < smaller.Length; i++)
+            {
+                if (smaller[i] != bigger[i])
+                    count++;
+            }
+
+            while (i < bigger.Length)
+            {
+                if (bigger[i] != '0')
+                    count++;
+                i++;
+            }
+
+
+            return count;
+        }
+
+        public char[] GetBits(int number)
+        {
+            string bits = string.Empty;
+
+            while (number > 0)
+            {
+                bits += number % 2;
+                number /= 2;
+            }
+
+            return bits.ToCharArray();
+        }
+    }
+}
---
+++ UnitTests/HammingDistanceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HammingDistanceTest
+    {
+        [TestMethod]
+        public void HammingDistanceTestMethod()
+        {
+            HammingDistance distance = new HammingDistance();
+            int value = distance.Solution(1, 400);
+            Assert.Equals(4, value);
+        }
+    }
+}
