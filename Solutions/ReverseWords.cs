0a990c70605d477c1f261a49b1fc4bbe78e336ab
faisal ahmed <mishu_dc@yahoo.com>
2018-02-10 00:19:25 -0500

    Reverse String (557)

---
+++ Solutions/ReverseWords.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+
+namespace Solutions
+{
+    public class ReverseWords
+    {
+        public string Execute(string s)
+        {
+            string[] words = s.Split(' ');
+            StringBuilder builder = new StringBuilder();
+            foreach (var word in words)
+            {
+                string reverse = ReverseString(word);
+                builder.Append(reverse);
+                builder.Append(" ");
+            }
+            string result = builder.ToString();
+            return result.Substring(0, result.Length - 1);
+        }
+
+        private string ReverseString(string word)
+        {
+            StringBuilder sb = new StringBuilder();
+            char[] characters = word.ToCharArray();
+            for(int i=characters.Length-1;i>=0;i--)
+            {
+                sb.Append(characters[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ UnitTests/ReverseWordsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ReverseWordsTest
+    {
+        [TestMethod]
+        public void ReverseWordsTestMethod1()
+        {
+            ReverseWords reverseWords = new ReverseWords();
+            string result = reverseWords.Execute("hello world");
+            Assert.AreEqual("olleh dlrow", result);
+        }
+    }
+}
