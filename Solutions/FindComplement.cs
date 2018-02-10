3f3ad6fade49723d7249bb849e90e56026f16ed7
faisal ahmed <mishu_dc@yahoo.com>
2018-02-09 23:47:06 -0500

    FindComplement

---
+++ Solutions/FindComplement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class FindComplement
+    {
+        public int Execute(int num)
+        {
+            int mask = ~0;
+            while ((mask & num) > 0) mask <<= 1;
+            return ~mask & ~num;
+        }
+    }
+}
