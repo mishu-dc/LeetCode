1f2dd98e554158124183289e8632356e73630319
faisal ahmed <mishu_dc@yahoo.com>
2018-02-09 23:08:41 -0500

    ArrayPairSum

---
+++ Solutions/ArrayPairSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class ArrayPairSum
+    {
+        public int Solution(int[] nums)
+        {
+            int sum = 0;
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i = i + 2)
+            {
+                sum += nums[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ UnitTests/ArrayPairSumTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ArrayPairSumTest
+    {
+        [TestMethod]
+        public void ArrayPairSumTestTestMethod1()
+        {
+            ArrayPairSum arrayPairSum = new ArrayPairSum();
+            int sum = arrayPairSum.Solution(new int[] { 1,2,3,4,5,6 });
+            Assert.AreEqual(9, sum);
+        }
+    }
+}
