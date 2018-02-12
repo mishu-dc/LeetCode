685bd7e3647f8e41c99d14bdde8c0b3815c0cdf7
faisal ahmed <mishu_dc@yahoo.com>
2018-02-11 21:43:52 -0500

    AddTwoNumbers2

---
+++ Solutions/AddTwoNumbers2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class AddTwoNumbers2
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int carry = 0;
+            Stack<int> fList = new Stack<int>();
+            Stack<int> sList = new Stack<int>();
+
+            while (l1 != null)
+            {
+                fList.Push(l1.val);
+                l1 = l1.next;
+            }
+
+            while (l2 != null)
+            {
+                sList.Push(l2.val);
+                l2 = l2.next;
+            }
+
+            ListNode head = null, tail = null;
+
+            do
+            {
+                if (fList.Count() == 0 && sList.Count() == 0) break;
+
+                int x = fList.Count > 0 ? fList.Pop() : 0;
+                int y = sList.Count > 0 ? sList.Pop() : 0;
+
+                int sum = x + y + carry;
+                carry = sum / 10;
+                sum = sum % 10;
+
+                if (tail == null)
+                {
+                    tail = new ListNode(sum);
+                    head = tail;
+                }
+                else
+                {
+                    head = new ListNode(sum);
+                    head.next = tail;
+                    tail = head;
+                }
+
+            } while (true);
+
+            if (carry > 0)
+            {
+                head = new ListNode(carry);
+                head.next = tail;
+                tail = head;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ UnitTests/AddTwoNumbers2Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AddTwoNumbers2Test : UnitTestBase
+    {
+        [TestMethod]
+        public void AddTwoNumbers2TestMethod1()
+        {
+            AddTwoNumbers2 nums = new AddTwoNumbers2();
+            ListNode res = nums.AddTwoNumbers(base.GetListNode(new int[] { 9, 9, 9, 9 }), base.GetListNode(new int[] { 9, 9, 9 }));
+            Assert.IsNotNull(res);
+            Assert.IsTrue(res.Equals(base.GetListNode(new int[] { 1, 0, 9, 9, 8 })));
+        }
+    }
+}
