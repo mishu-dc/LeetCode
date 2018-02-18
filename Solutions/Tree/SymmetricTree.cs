e8a393e15d4d71339af305202f75755077f286c2
faisal ahmed <mishu_dc@yahoo.com>
2018-02-18 10:50:48 -0500

    Symmetric Tree

---
+++ Solutions/Tree/SymmetricTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class SymmetricTree
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+            return recure(root.left, root.right);
+        }
+
+        public bool recure(TreeNode left, TreeNode right)
+        {
+            if (left == null || right == null)
+                return left == right;
+            if (left.val != right.val)
+                return false;
+            return recure(left.left, right.right) && recure(left.right, right.left);
+        }
+    }
+}
---
+++ UnitTests/Tree/SymmetricTreeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SymmetricTreeTest: UnitTestBase
+    {
+        [TestMethod]
+        public void SymmetricTreeTestMethod1()
+        {
+            SymmetricTree tree = new SymmetricTree();
+            bool result = tree.IsSymmetric(base.GetTreeNode(new int?[] { 1, 2, 2, 3, 4, 4, 3 }));
+            Assert.AreEqual(true, result);
+        }
+    }
+}
