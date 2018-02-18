cc78b883cbed513cca209f950e9ad63d3b9a55ca
faisal ahmed <mishu_dc@yahoo.com>
2018-02-18 10:50:00 -0500

    Palindrom Linked List

---
+++ Solutions/LinkList/PalindromeLinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class PalindromeLinkedList
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            int length = 0;
+            int mid = 0;
+            int count = 1;
+
+            ListNode temp = head;
+            ListNode midPoint = null;
+
+            while (temp != null)
+            {
+                temp = temp.next;
+                length++;
+            }
+
+            mid = (length / 2 + 1);
+
+            midPoint = head;
+            while (count < mid)
+            {
+                midPoint = midPoint.next;
+                count++;
+            }
+
+            midPoint = ReverseList(midPoint);
+
+            count = 0;
+            while (count < mid)
+            {
+                if (head.val != midPoint.val)
+                    return false;
+                count++;
+                head = head.next;
+                midPoint = midPoint.next;
+            }
+
+            return true;
+        }
+
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode reversed = null;
+            ListNode temp = null;
+            while (head != null)
+            {
+                temp = new ListNode(head.val);
+                if (reversed != null)
+                {
+                    temp.next = reversed;
+                }
+                reversed = temp;
+                head = head.next;
+            }
+            return reversed;
+        }
+    }
+}
---
+++ UnitTests/LinkList/PalindromeLinkedListTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PalindromeLinkedListTest : UnitTestBase
+    {
+        [TestMethod]
+        public void PalindromeLinkedListTestMethod1()
+        {
+            PalindromeLinkedList list = new PalindromeLinkedList();
+            bool result = list.IsPalindrome(base.GetListNode(new int[] { 1, 2, 3, 2, 1 }));
+            Assert.AreEqual(true, result);
+        }
+    }
+}
