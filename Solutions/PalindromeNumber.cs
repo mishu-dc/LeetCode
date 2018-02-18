b626f2f498745dab02ced6216036e419ec14e662
faisal ahmed <mishu_dc@yahoo.com>
2018-02-18 10:49:38 -0500

    Palindrome Number

---
+++ Solutions/PalindromeNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            long number = x;
+            number = Math.Abs(number);
+            long hfactor = 1;
+            long lfactor = 10;
+
+            while (number / hfactor > 0)
+            {
+                hfactor = hfactor * 10;
+            }
+
+            hfactor = hfactor / 10;
+
+            number = x;
+            number = Math.Abs(number);
+            while (hfactor > 0)
+            {
+                if (number % lfactor != number / hfactor)
+                    return false;
+                number = number % hfactor;
+                number = number / lfactor;
+                hfactor = hfactor / 100;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnitTests/PalindromeNumberTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PalindromeNumberTest
+    {
+        [TestMethod]
+        public void PalindromeNumberTestMethod1()
+        {
+            PalindromeNumber number = new PalindromeNumber();
+            bool result = number.IsPalindrome(-2147483648);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
